16344fa6d1ce405d03c70faf7d25e99b9431c657
leahthompson01 <leahmthompson03@gmail.com>
2022-08-24 02:08:58 -0400

    fixed division issues. Answers now closer in range

--- server/server.py
+++ server/server.py
@@ -41,37 +41,56 @@ class Question:
             self.operand = 'minus'
         if(operator == 'multiplication'):
             self.rightAnswer = num1 * num2
+            print(self.rightAnswer)
             self.operand = 'times'
         if(operator == 'division'):
-            self.rightAnswer = round(num1 / num2, 2)
+            num1 = find_evenly_divisible(num1, num2)
+            num2 = makeNum2Pos(num2)
+            self.rightAnswer = round(num1/num2)
             self.operand = 'divided by'
         self.question = 'What is the result ' + \
             ' of ' + str(num1) + ' ' + self.operand + ' ' + str(num2) + '?'
         self.answerChoices = AnswerChoices(self.rightAnswer, operator)
 
+# if num2 is 0, this function will return a non zero value
+# necessary because we cannot divide by 0 (undefined)
+
+
+def makeNum2Pos(num2):
+    while(num2 == 0):
+        num2 = random.randrange(-25, 25)
+    return num2
+
+
+def find_evenly_divisible(num1, num2):
+
+    if(num1 % num2 != 0):
+        num1 = num2 * random.randint(1, 25)
+    return num1
+
 
 class AnswerChoices:
     def __init__(self, rightAnswer, operator):
-        random1 = random.randint(-100, 100)
-        random2 = random.randint(-100, 100)
-        random3 = random.randint(-100, 100)
-        if(operator == 'division'):
-            randomQuotient1 = round(random1/random3, 2)
-            randomQuotient2 = round(random2/random1, 2)
-            randomQuotient3 = round(random3/random2, 2)
-            if(randomQuotient1 != rightAnswer and randomQuotient2 != rightAnswer and randomQuotient3 != rightAnswer):
-                if(randomQuotient1 != randomQuotient2 and randomQuotient2 != randomQuotient3 and randomQuotient1 != randomQuotient3):
-                    self.answerChoice1 = randomQuotient1
-                    self.answerChoice2 = randomQuotient2
-                    self.answerChoice3 = round(rightAnswer, 2)
-                    self.answerChoice4 = randomQuotient3
-        if(operator != 'division'):
-            if(random1 != rightAnswer and random2 != rightAnswer and random3 != rightAnswer):
-                if(random1 != random2 and random2 != random3 and random1 != random3):
-                    self.answerChoice1 = random1
-                    self.answerChoice2 = random2
-                    self.answerChoice3 = rightAnswer
-                    self.answerChoice4 = random3
+
+        # if(operator == 'division'):
+        #     if(random1 != rightAnswer and random2 != rightAnswer and random3 != rightAnswer):
+        #         if(random1 != random2 and random2 != random3 and random1 != random3):
+        #             if(random1 != 0 and random2 != 0 and random3 != 0):
+        #                 self.answerChoice1 = random1/random2
+        #                 self.answerChoice2 = random2/random3
+        #                 self.answerChoice3 = rightAnswer
+        #                 self.answerChoice4 = random3/random1
+
+        # if(operator != 'division'):
+        random1 = random.randrange(rightAnswer-10, rightAnswer+10)
+        random2 = random.randrange(rightAnswer-10, rightAnswer+10)
+        random3 = random.randrange(rightAnswer-10, rightAnswer+10)
+        if(random1 != rightAnswer and random2 != rightAnswer and random3 != rightAnswer):
+            if(random1 != random2 and random2 != random3 and random1 != random3):
+                self.answerChoice1 = random1
+                self.answerChoice2 = random2
+                self.answerChoice3 = rightAnswer
+                self.answerChoice4 = random3
             # need to have some negative answer choices
 
 
@@ -85,8 +104,8 @@ async def root():
 async def createQuiz(operation: str = "addition"):
     allQuestions = []
     for x in range(0, 10):
-        randomNum1 = random.randint(-100, 100)
-        randomNum2 = random.randint(-100, 100)
+        randomNum1 = random.randint(-25, 25)
+        randomNum2 = random.randint(-25, 25)
         indivQuestion = Question(operation, randomNum1, randomNum2)
         allQuestions.append(indivQuestion)
 
