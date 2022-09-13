83bbeb10fa8220538c77af9b2246b5bda740740e
leahthompson01 <leahmthompson03@gmail.com>
2022-09-12 22:29:19 -0400

    updated with comments for initial websocket code

--- server/server.py
+++ server/server.py
@@ -1,4 +1,6 @@
 import random
+import socket
+import threading
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -54,6 +56,11 @@ class Question:
 
 # if num2 is 0, this function will return a non zero value
 # necessary because we cannot divide by 0 (undefined)
+
+
+# is the max number of people who can connect to a server at a time
+LISTENER_LIMIT = 5
+active_clients = []
 
 
 def makeNum2Pos(num2):
@@ -129,3 +136,76 @@ async def createQuiz(operation: str = "addition"):
         allQuestions.append(indivQuestion)
 
     return allQuestions
+
+
+# def listen_for_messages(client, username):
+
+#     while 1:
+
+#         message = client.recv(2048).decode('utf-8')
+#         if message != '':
+
+#             final_msg = username + '~' + message
+#             send_messages_to_all(final_msg)
+
+#         else:
+#             print(f"The message send from client {username} is empty")
+
+
+# def send_message_to_client(client, message):
+
+#     client.sendall(message.encode())
+
+
+# def send_messages_to_all(message):
+
+#     for user in active_clients:
+
+#         send_message_to_client(user[1], message)
+
+
+# def client_handler(client):
+
+#     # Server will listen for client message that will
+#     # Contain the username
+#     while 1:
+
+#         username = client.recv(2048).decode('utf-8')
+#         if username != '':
+#             active_clients.append((username, client))
+#             prompt_message = "SERVER~" + f"{username} added to the chat"
+#             # send_messages_to_all(prompt_message)
+#             print('added new client')
+#             break
+#         else:
+#             print("Client username is empty")
+
+#     threading.Thread(target=listen_for_messages, args=(client,
+#                                                        username, )).start()
+# # //creates the websocket
+# # in client, need to run main whenever
+
+
+# def main():
+
+#     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     global HOST
+#     global PORT
+#     try:
+#         server.bind((HOST, PORT))
+#         print(f"Running the server on {HOST} {PORT}")
+#     except:
+#         print(f"Unable to bind to host {HOST} and port {PORT}")
+
+#     server.listen(LISTENER_LIMIT)
+
+#     while 1:
+
+#         client, address = server.accept()
+#     print(f"Successfully connected to client {address[0]} {address[1]}")
+
+#     threading.Thread(target=client_handler, args=(client, )).start()
+
+
+# if __name__ == '__main__':
+#     main()
